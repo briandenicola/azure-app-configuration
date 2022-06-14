6598b250a0656898e105487a089d091ad3d68c75
briandenicola <brian@briandenicola.com>
2022-06-14 13:01:46 -0500

    Q&D Example

---
+++ private-endpoint/src/GlobalUsing.cs
@@ -0,0 +1,5 @@
+global using Azure.Core;
+global using Azure.Identity;
+global using Azure.Data.AppConfiguration;
+global using System.CommandLine;
+global using System.CommandLine.NamingConventionBinder;
---
+++ private-endpoint/src/Program.cs
@@ -0,0 +1,34 @@
+﻿ void RunMain (Uri appConfigUri)
+ {   
+    var credential = new ChainedTokenCredential(new EnvironmentCredential(), new AzureCliCredential());
+    var client = new ConfigurationClient(appConfigUri, credential);
+    ConfigurationSetting setting = client.GetConfigurationSetting("sample");
+    System.Console.WriteLine($"The value of \"sample\" retrieved from {appConfigUri} is \"{setting.Value}\"");
+}
+
+RootCommand command = new RootCommand("A basic example using Azure AppConfig")
+{
+    new Option<Uri>(
+        aliases: new [] {"--app-config", "-a"},
+        description: "App Config Uri or Name, e.g. my-appconfig or https://my-appconfig.azconfig.io",
+        parseArgument: result =>
+        {
+            string value = result.Tokens.Single().Value;
+            if (Uri.TryCreate(value, UriKind.Absolute, out Uri appConfigUri) ||
+                Uri.TryCreate($"https://{value}.azconfig.io", UriKind.Absolute, out appConfigUri))
+            {
+                return appConfigUri;
+            }
+
+            result.ErrorMessage = "Must specify a vault name or URI";
+            return null!;
+        }
+    )
+    {
+        Name = "appConfigUri",
+        IsRequired = true,
+    }
+};
+
+command.Handler = CommandHandler.Create<Uri>(RunMain);
+return command.Invoke(args);
